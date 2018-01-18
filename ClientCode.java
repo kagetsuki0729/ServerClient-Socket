7f298838151ce6fee6c319c8df4f89682b3e6581
kagetsuki0729 <16396734+kagetsuki0729@users.noreply.github.com>
2018-01-18 10:36:15 +0800

    Update ClientCode.java

--- ClientCode.java
+++ ClientCode.java
@@ -2,7 +2,6 @@ public class ClientCode
 {
     public static void main(String[] argv)
     {
-        new ClientThreadCode("34.216.142.71", 9999).start();//建立物件，傳入IP和Port並執行等待接受連線的動作
-        //由於此範例都在自己電腦上執行，所以IP設定為127.0.0.1
+        new ClientThreadCode("34.216.142.71", 9999).start();
     }
 }
